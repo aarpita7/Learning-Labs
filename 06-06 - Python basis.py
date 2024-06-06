ac7c5c6a8fc844a800b88e869cba60faaea801a8
Arpita Mahapatra <151190372+aarpita7@users.noreply.github.com>
2024-06-06 15:15:14 +0530

    Add files via upload

---
+++ 06-06 - Python basis.py
@@ -0,0 +1,10 @@
+# Data Types in Python
+int_num = 10
+float_num = 3.14
+complex_num = 2 + 3j
+string = "Hello, World!"
+boolean = True
+list_example = [1, 2, 3, 4, 5]
+tuple_example = (1, 2, 3, 4, 5)
+dict_example = {"name": "John", "age": 30}
+set_example = {1, 2, 3, 4, 5}
